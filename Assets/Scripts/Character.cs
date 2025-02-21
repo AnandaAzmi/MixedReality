04d5a40b14fd05b904512c63a0dc3086bf39ee95
Ananda Syafakhatul Azmi <anandaazmi2907@gmail.com>
2025-02-21 22:50:23 +0700

    init project

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Character : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            animator.SetBool("isAttacking", true);
+            GameObject go = Instantiate(bulletPrefab,spawnPoint1.position, spawnPoint1.rotation) as GameObject;
+            go.GetComponent<Rigidbody>().AddForce(transform.forward*20, ForceMode.VelocityChange);
+            GameObject go1 = Instantiate(bulletPrefab, spawnPoint2.position, spawnPoint2.rotation) as GameObject;
+            go1.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.VelocityChange);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+}
