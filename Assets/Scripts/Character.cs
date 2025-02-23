7345ea65f02a0f641891cbf1fa72558ab5bb2d74
Ananda Syafakhatul Azmi <anandaazmi2907@gmail.com>
2025-02-23 11:38:50 +0700

    ga jadi shooter

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -14,13 +14,13 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             animator.SetBool("isAttacking", true);
-            GameObject go = Instantiate(bulletPrefab,spawnPoint1.position, spawnPoint1.rotation) as GameObject;
-            go.GetComponent<Rigidbody>().AddForce(transform.forward*20, ForceMode.VelocityChange);
+            GameObject go = Instantiate(bulletPrefab, spawnPoint1.position, spawnPoint1.rotation) as GameObject;
+            go.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.VelocityChange);
             GameObject go1 = Instantiate(bulletPrefab, spawnPoint2.position, spawnPoint2.rotation) as GameObject;
-            go1.GetComponent<Rigidbody>().AddForce(transform.forward * 20, ForceMode.VelocityChange);
+            go1.GetComponent<Rigidbody>().AddForce(transform.forward * 15, ForceMode.VelocityChange);
         }
         else
         {
